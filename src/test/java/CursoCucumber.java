dc363826cc72edf47319b1858c408402b75e9fec
lucas-da-cruz <50348175+lucas-da-cruz@users.noreply.github.com>
2020-07-27 21:33:49 -0300

    New scenarios
    Novos cenários com diferentes expressões regulares adicionadas

--- src/test/java/CursoCucumber.java
+++ src/test/java/CursoCucumber.java
@@ -2,6 +2,11 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
 import cucumber.api.PendingException;
@@ -43,5 +48,37 @@ public class CursoCucumber {
 	public void o_valor_do_contador_ser(int arg1) throws Throwable {
 		Assert.assertEquals(arg1, contador);
 	}
+	
+	Date entrega = new Date();
+	
+	@Given("^que a entrega eh dia (\\d+)/(\\d+)/(\\d+)$")
+	public void que_a_entrega_eh_dia(int day, int month, int year) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, day);
+		cal.set(Calendar.MONTH, month-1);
+		cal.set(Calendar.YEAR, year);
+		entrega = cal.getTime();
+	}
+
+	@When("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void a_entrega_atrasar_em_dias(int arg1, String tempo) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		if(tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+		}
+		if(tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, arg1);
+		}
+		entrega = cal.getTime();
+	}
+
+	@Then("^a entrega sera efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void a_entrega_sera_efetuada_em(String data) throws Throwable {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
+	}
+
 
 }
--- src/test/java/curso_cucumber.feature
+++ src/test/java/curso_cucumber.feature
@@ -8,8 +8,24 @@ Feature: Aprender cucumber
     When executa-lo
     Then a especificacao deve finalizar com sucesso
     
-  Scenario: Deve incrementar contador
+Scenario: Deve incrementar contador
   	Given que o valor do contador eh 15
   	When eu incrementar em 5
   	Then o valor do contador sera 20
+  	
+Scenario: Deve incrementar contador
+  	Given que o valor do contador eh 123
+  	When eu incrementar em 35
+  	Then o valor do contador sera 158
+  	
+Scenario: Deve calcular atraso na entrega
+	Given que a entrega eh dia 05/04/2018
+	When a entrega atrasar em 2 dias
+	Then a entrega sera efetuada em 07/04/2018
+	
+Scenario: Deve calcular atraso no prazo de entrega da china
+	Given que a entrega eh dia 05/04/2018
+	When a entrega atrasar em 2 meses
+	Then a entrega sera efetuada em 05/06/2018
+	
   	
